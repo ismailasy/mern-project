b1b1c8a6f40b6e954c367f3e308aa111e57f7b57
ismailasy <baboyesy10@gmail.com>
2023-07-13 15:03:22 +0000

    first commit

---
+++ routes/userRoutes.js
@@ -0,0 +1,66 @@
+const express = require('express');
+const router = express.Router();
+const User = require('../models/User');
+
+// GET all users
+router.get('/', async (req, res) => {
+  try {
+    const users = await User.find();
+    res.json(users);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+// GET a specific user
+router.get('/:id', async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    res.json(user);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+// POST a new user
+router.post('/', async (req, res) => {
+  const user = new User({
+    username: req.body.username,
+    email: req.body.email,
+    password: req.body.password
+  });
+
+  try {
+    const newUser = await user.save();
+    res.status(201).json(newUser);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
+
+// PUT update a user
+router.put('/:id', async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    if (req.body.username) user.username = req.body.username;
+    if (req.body.email) user.email = req.body.email;
+    if (req.body.password) user.password = req.body.password;
+    const updatedUser = await user.save();
+    res.json(updatedUser);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
+
+// DELETE a user
+router.delete('/:id', async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    const deletedUser = await user.remove();
+    res.json(deletedUser);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const app = express();
+
+//importtion des routes
+const userRoutes = require('./routes/userRoutes');
+
+// Set up middleware
+app.use(bodyParser.json());
+
+
+
+// Register the routes
+app.use('/api/users', userRoutes);
+
+
+// Connect to MongoDB
+mongoose.connect('mongodb://localhost/mernstack', { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((error) => {
+    console.error('Error connecting to MongoDB', error);
+  });
+
+// Start the server
+const port = 5000;
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
